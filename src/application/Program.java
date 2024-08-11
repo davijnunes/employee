ca88bf5e23678de81fa80c219eec8fc73b04b5bc
Davi Nunes <sallyn2544@gmail.com>
2024-08-11 13:28:48 -0300

    We added the logic for creating specific employees and do not allow identical ids

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,8 +20,59 @@ public class Program {
 		
 		List<Employee> listOfEmployee = new ArrayList<Employee>();
 		
+		System.out.print("Enter a quantity of employee: ");
+		int quantityOfEmployee = sc.nextInt();
+		
+		System.out.println();
+		
+		for(int i = 0; i < quantityOfEmployee; i++) {
+			
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			
+			// System.out.println("Debugging: " + hasId(listOfEmployee,id));
+			
+			while(hasId(listOfEmployee,id)) {
+				
+				// System.out.println("Debugging");
+				System.out.println("Enter new id please: ");
+				id = sc.nextInt();
+			}
+			
+			System.out.print("Name: ");
+			// clear buffer
+			sc.nextLine();
+			String name = sc.nextLine();
+			
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			// clear buffer
+			sc.nextLine();
+			
+			System.out.print("Employee is of type manager or programmer: ");
+			String typeOfProgrammer = sc.nextLine();
+			
+			if(typeOfProgrammer.equalsIgnoreCase("manager")) {
+				
+				Employee employee = new EmployeeManager(name,salary,id);
+				listOfEmployee.add(employee);
+			}
+			else if(typeOfProgrammer.equalsIgnoreCase("programmer")) {
+				
+				Employee employee = new EmployeeProgrammer(name, salary, id);
+				listOfEmployee.add(employee);
+			}
+		}
 		
 		sc.close();
+	}
+	
+	public static boolean hasId(List<Employee> list, Integer id) {
+	
+		Employee emp = list.stream().filter(obj -> obj.getId().equals(id)).findFirst().orElse(null);
+		
+		return emp != null;
 	}
 
 }
